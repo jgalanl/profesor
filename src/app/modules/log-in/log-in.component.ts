b87258deae895625c3e7ff4873b2ec32b17fbc0c
16jumila <julissa.gutierrezva@gmail.com>
2020-04-30 11:58:50 +0200

    login

--- src/app/modules/log-in/log-in.component.ts
+++ src/app/modules/log-in/log-in.component.ts
@@ -1,52 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
-import { UsuariosService } from 'src/app/services/usuarios.service';
-import { Subscription } from 'rxjs';
-import { User } from 'src/app/data/user';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
+import { UsuariosService } from "src/app/services/usuarios.service";
+import { Subscription } from "rxjs";
+import { User } from "src/app/data/user";
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-log-in',
-  templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.scss']
+  selector: "app-log-in",
+  templateUrl: "./log-in.component.html",
+  styleUrls: ["./log-in.component.scss"],
 })
 export class LogInComponent implements OnInit {
-
-  public user: User
-  public s_user: Subscription
+  public user: User;
+  public s_user: Subscription;
 
   loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(5)])
-  })
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+  });
 
-  constructor(private dialog: MatDialog, 
-    private router: Router, private auth: AuthService, private us: UsuariosService) { 
+  constructor(
+    private dialog: MatDialog,
+    private router: Router,
+    private auth: AuthService,
+    private us: UsuariosService
+  ) {}
 
-  }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSubmit() {
-    this.auth.login(this.loginForm.get('email').value, this.loginForm.get('password').value)
-    .then(res => {
-      this.us.getUserByEmail(this.loginForm.get('email').value)
-      .then(res => {
-        if(res.rol === 'profesor'){
-          this.router.navigate(['/home-profesor'], {state : res});
-        }
-        else{
-          this.router.navigate(['/home-alumno'], {state : res} )
-        }       
+    this.auth
+      .login(
+        this.loginForm.get("email").value,
+        this.loginForm.get("password").value
+      )
+      .then((res) => {
+        this.us
+          .getUserByEmail(this.loginForm.get("email").value)
+          .then((res) => {
+            if (res.rol === "Profesor") {
+              this.router.navigate(["/home-profesor"], { state: res });
+            } else {
+              this.router.navigate(["/home-alumno"], { state: res });
+            }
+          });
       })
-    })
-    .catch(err => {
-      //TODO. Incluir dialog
-      window.alert("Usuario o contraseña incorrectos")
-    })
- }
-
+      .catch((err) => {
+        //TODO. Incluir dialog
+        window.alert("Usuario o contraseña incorrectos");
+      });
+  }
 }
